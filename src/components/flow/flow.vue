9fddc6df477dbdd3540f81de7ed86a0faabc9acf
leaderli <429243408@qq.com>
2021-11-23 17:29:52 +0800

    vue-shape

--- src/components/flow/flow.vue
+++ src/components/flow/flow.vue
@@ -1,28 +1,38 @@
 <template>
     <div>
-        <!-- <el-button @click="add()">内部Add: {{ num }}</el-button> -->
+        <el-button @click="add()">内部Add: {{ num }}</el-button>
     </div>
 </template>
 
 <script lang="ts" setup>
 import { Graph } from '@antv/x6';
-import { inject } from 'vue';
-const x6 = inject("getGraph", "getNode") as any as Graph
-let num = 0;
-// const node = x6.getNode() ;
+import { ref, inject } from 'vue';
+const x6 = inject("node", "getNode") as any
+console.log(x6);
+console.log(x6.getNode);
+
+
+let num = ref(0);
+// const node = x6.getNode();
 
 // // 监听数据改变事件
 // node.on("change:data", ({ current }) => {
 //     num = current.num;
 // });
 
+// console.log(x6);
+// console.log(x6.getNode);
+
 // methods: {
-//     add() {
-//         const node = this.getNode();
-//         const { num } = node.getData();
-//         node.setData({
-//             num: num + 1,
-//         });
-//     },
+function add() {
+    // const node = x6.getNode();
+    // const { num } = node.getData();
+    // node.setData({
+    // num: num + 1,
+    // });
+    console.log('add', num.value);
+
+    num.value = num.value + 1
+}
 // },
 </script>
--- src/views/flow/index.vue
+++ src/views/flow/index.vue
@@ -1,5 +1,6 @@
-<template>
+<!-- <template>
     <div class="app-container documentation-container">
+        <flow></flow>
         <div id="container" />
     </div>
 </template>
@@ -7,24 +8,27 @@
 <script setup lang="ts">
 console.log('----------------------------');
 
-import { onMounted } from 'vue';
 import { Graph } from '@antv/x6'
+import '@antv/x6-vue-shape'
+import flow from '@/components/flow/flow.vue'
+
+import { onMounted, provide } from 'vue';
 // import { ToolsView } from "/node_modules/@antv/x6/src/view/tool.ts";
 
 // console.log(ToolsView);
 
+provide('node', 'test1')
 
 
-// import '@antv/x6-vue3-shape'
 
-// import flow from '@/components/flow/flow.vue'
+
 
 
 console.log('----------------------------------------');
 
 // console.log(container);
 console.log('----------------------------------------');
-// const data = { num: 0 }
+const data1 = { num: 0 }
 // const graph = new Graph({
 //     // container: container as any,
 //     width: 600,
@@ -32,109 +36,9 @@ console.log('----------------------------------------');
 //     grid: true,
 // });
 
-// graph.addNode({
-//     shape: 'vue-shape',
-//     width: 200,
-//     height: 200,
-//     x: 100,
-//     y: 100,
-//     attrs: {
-//         body: {
-//             width: 200,
-//             height: 200,
-//             stroke: 'red',
-//         }
-//     },
-//     component: {
-//         template: `<flow :num="num" @add="add()"></flow>`,
-//         data() {
-//             return data
-//         },
-//         methods: {
-//             add: () => {
-//                 data.num += 1
-//             }
-//         },
-//         components: {
-//             flow,
-//         }
-//     }
-// });
-// const init = function (container: HTMLElement) {
-//     const graph = new Graph({
-//         container: container,
-//         width: 1000,
-//         height: 500,
-//         background: {
-//             color: 'rgba(184,180,180,0.2)' // 设置画布背景颜色
-//         },
-//         grid: {
-//             size: 10, // 网格大小 10px
-//             visible: true // 渲染网格背景
-//         }
-//     })
-//     const data = {
-//         // 节点
-//         nodes: [
-//             {
-//                 id: 'node1', // String，可选，节点的唯一标识
-//                 x: 40, // Number，必选，节点位置的 x 值
-//                 y: 40, // Number，必选，节点位置的 y 值
-//                 width: 80, // Number，可选，节点大小的 width 值
-//                 height: 40, // Number，可选，节点大小的 height 值
-//                 label: '普线' // String，节点标签
-//             },
-//             {
-//                 id: 'node2', // String，节点的唯一标识
-//                 x: 160, // Number，必选，节点位置的 x 值
-//                 y: 80, // Number，必选，节点位置的 y 值
-//                 width: 80, // Number，可选，节点大小的 width 值
-//                 height: 40, // Number，可选，节点大小的 height 值
-//                 label: '夜间' // String，节点标签
-//             },
-//             {
-//                 id: 'node3', // String，节点的唯一标识
-//                 x: 160, // Number，必选，节点位置的 x 值
-//                 y: 180, // Number，必选，节点位置的 y 值
-//                 width: 80, // Number，可选，节点大小的 width 值
-//                 height: 40, // Number，可选，节点大小的 height 值
-//                 attrs: {
-//                     body: {
-//                         fill: '#fbca00',
-//                         stroke: '#000',
-//                         strokeDasharray: '10,2'
-//                     },
-//                     label: {
-//                         text: '白金',
-//                         fill: '#333',
-//                         fontSize: 13
-//                     }
-//                 }
-//             }
-//         ],
-//         // 边
-//         edges: [
-//             {
-//                 source: 'node1', // String，必须，起始节点 id
-//                 target: 'node2' // String，必须，目标节点 id
-//             },
-//             {
-//                 source: 'node1', // String，必须，起始节点 id
-//                 target: 'node3' // String，必须，目标节点 id
-//             }
-//         ]
-//     }
-//     graph.fromJSON(data)
-// }
-
-
-
-var init = () => {
-    console.log('hello');
-    const container = document.getElementById('container')
-    console.log(container);
+const init = function (container: HTMLElement) {
     const graph = new Graph({
-        container: container as HTMLElement,
+        container: container,
         width: 1000,
         height: 500,
         background: {
@@ -145,40 +49,96 @@ var init = () => {
             visible: true // 渲染网格背景
         }
     })
-    // 从 node_modules 引入
-    // 从 CND 引入时，我们暴露了 X6 这个全局变量
-    // const { Graph } = X6
+    const data = {
+        // 节点
+        nodes: [
+            {
+                id: 'node1', // String，可选，节点的唯一标识
+                x: 40, // Number，必选，节点位置的 x 值
+                y: 40, // Number，必选，节点位置的 y 值
+                width: 80, // Number，可选，节点大小的 width 值
+                height: 40, // Number，可选，节点大小的 height 值
+                label: '普线' // String，节点标签
+            },
+            {
+                id: 'node2', // String，节点的唯一标识
+                x: 160, // Number，必选，节点位置的 x 值
+                y: 80, // Number，必选，节点位置的 y 值
+                width: 80, // Number，可选，节点大小的 width 值
+                height: 40, // Number，可选，节点大小的 height 值
+                label: '夜间' // String，节点标签
+            },
+            {
+                id: 'node3', // String，节点的唯一标识
+                x: 160, // Number，必选，节点位置的 x 值
+                y: 180, // Number，必选，节点位置的 y 值
+                width: 80, // Number，可选，节点大小的 width 值
+                height: 40, // Number，可选，节点大小的 height 值
+                attrs: {
+                    body: {
+                        fill: '#fbca00',
+                        stroke: '#000',
+                        strokeDasharray: '10,2'
+                    },
+                    label: {
+                        text: '白金',
+                        fill: '#333',
+                        fontSize: 13
+                    }
+                }
+            }
+        ],
+        // 边
+        edges: [
+            {
+                source: 'node1', // String，必须，起始节点 id
+                target: 'node2' // String，必须，目标节点 id
+            },
+            {
+                source: 'node1', // String，必须，起始节点 id
+                target: 'node3' // String，必须，目标节点 id
+            }
+        ]
+    }
+    graph.fromJSON(data)
 
-    // 创建 Graph 的实例
-
-    // 渲染源节点
-    const source = graph.addNode({
-        x: 300,
-        y: 40,
-        width: 80,
-        height: 40,
-        label: 'Hello',
-    })
-
-    // 渲染目标节点
-    const target = graph.addNode({
-        x: 420,
-        y: 180,
-        width: 80,
-        height: 40,
-        label: 'World',
-    })
-
-    // 渲染边
-    graph.addEdge({
-        source,
-        target,
-    })
-
-
+    graph.addNode({
+        shape: 'vue-shape',
+        width: 200,
+        height: 200,
+        x: 100,
+        y: 100,
+        attrs: {
+            body: {
+                width: 200,
+                height: 200,
+                stroke: 'red',
+            }
+        },
+        component: {
+            template: `<flow :num="num" @add="add()"></flow>`,
+            data() {
+                return data1
+            },
+            methods: {
+                add: () => {
+                    data1.num += 1
+                }
+            },
+            provide: {
+                node: 'test2'
+            },
+            components: {
+                flow,
+            }
+        }
+    });
 }
-onMounted(()=>{
-    init()
+
+
+onMounted(() => {
+    const container = document.getElementById('container')
+    init(container as HTMLElement)
 })
 </script>
 
@@ -205,4 +165,67 @@ onMounted(()=>{
 }
 </style>
 
+ -->
 
+
+
+<template>
+    <div>
+        <div id="container"></div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { Graph } from "@antv/x6";
+import "@antv/x6-vue-shape";
+import Count from "@/components/flow/flow.vue";
+import { onMounted } from 'vue';
+
+let graph = null;
+onMounted(() => {
+    graph = new Graph({
+        container: document.getElementById("container") as HTMLElement,
+        width: 600,
+        height: 400,
+        grid: true,
+    });
+
+    // 注册 vue component
+    // 如果需要序列化/反序列化数据，必须使用该方式
+    Graph.registerVueComponent(
+        "count",
+        {
+            template: `<Count />`,
+            components: {
+                Count,
+            },
+        },
+        true
+    );
+
+    graph.addNode({
+        id: "1",
+        shape: "vue-shape",
+        x: 200,
+        y: 150,
+        width: 150,
+        height: 100,
+        component: "count",
+        data: {
+            num: 0,
+        },
+    });
+})
+
+</script>
+
+<style>
+#container {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
